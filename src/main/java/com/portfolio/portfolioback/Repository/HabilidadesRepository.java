b979a8e6dd32341e106acc574ac3c11a94b77205
yusver1 <yusverortega@gmail.com>
2023-06-27 16:09:34 -0300

    errores con la base

--- src/main/java/com/portfolio/portfolioback/Repository/HabilidadesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portfolio.portfolioback.Repository;
-
-
-import com.portfolio.portfolioback.Entity.Habilidades;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HabilidadesRepository  extends CrudRepository<Habilidades, Integer> {
-}
